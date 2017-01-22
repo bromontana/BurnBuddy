fa29e2dd90e704e855c7570e2881a41f1bb2ad0d
barrios jesus <barrios jesus>
2017-01-21 23:32:08 -0500

    Defined check image function

--- burnbuddy.py
+++ burnbuddy.py
@@ -1,17 +1,26 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 
+imageTemp = ""
+
+#Definging keys and stuff
 app = ClarifaiApp("WQ9TJHAvPg5lsfAnKGXDqoygEAJWAOWzEFTkV-gW", "aoVEstd0WjXLEoJSE3rhi4LO1woDKWLUA-s-8enM")
+#Somewhere here the twilio stuff will happen
 
-model = app.models.get("burns")
+def checkImage(imageTemp):
+    #Pulling a model that we already trained
+    model = app.models.get("burns")
+    
+    #storing ALL the info from the predict for image
+    imageResponse = model.predict_by_filename(imageTemp)
 
-imageResponse = model.predict_by_filename("C:/Users/thepr/Documents/Hackathons/BurnBuddy/second1.jpg")
+    print (imageResponse['outputs'][0]['data']['concepts'][0]['id'] ,':',  imageResponse['outputs'][0]['data']['concepts'][0]['value'])
 
-print (imageResponse['outputs'][0]['data']['concepts'][0]['id'] ,':',  imageResponse['outputs'][0]['data']['concepts'][0]['value'])
-
-if ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'not burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.5)):
-    print("Don't be a wuss")
-elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'first degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
-    print("Slap an ice pack on that bitch")    
-elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'second degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
-    print("Slather your arm in cream cheese and go see a doctor yo!")       
+    if ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'not burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.5)):
+        print("Don't be a wuss")
+    elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'first degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
+        print("Slap an ice pack on that bitch")    
+    elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'second degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
+        print("Slather your arm in cream cheese and go see a doctor yo!")       
+    else:
+        print("Based on our results you should seek medical attention")
