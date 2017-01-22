b6af1c2c77e819d29ae0f8993491c596f9c7339f
Jessie Pullaro <jessiepullaro@Jessies-MacBook-Air.local>
2017-01-21 23:34:51 -0500

    added the twilio function

--- burnbuddy.py
+++ burnbuddy.py
@@ -1,17 +1,55 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
+from twilio.rest import TwilioRestClient
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def tImage():
+    # Your Account Sid and Auth Token from twilio.com/user/account
+    account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    auth_token  = "your_auth_token"
+    client = TwilioRestClient(account_sid, auth_token)
+
+    message = client.messages.get("SID_HERE")
+    clarifai_Image = message.body
+
+
 
 app = ClarifaiApp("WQ9TJHAvPg5lsfAnKGXDqoygEAJWAOWzEFTkV-gW", "aoVEstd0WjXLEoJSE3rhi4LO1woDKWLUA-s-8enM")
 
 model = app.models.get("burns")
 
-imageResponse = model.predict_by_filename("C:/Users/thepr/Documents/Hackathons/BurnBuddy/second1.jpg")
+imageResponse = model.predict_by_filename(clarifai_Image)
 
 print (imageResponse['outputs'][0]['data']['concepts'][0]['id'] ,':',  imageResponse['outputs'][0]['data']['concepts'][0]['value'])
 
 if ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'not burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.5)):
     print("Don't be a wuss")
 elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'first degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
-    print("Slap an ice pack on that bitch")    
+    print("Slap an ice pack on that bitch")
 elif ( imageResponse['outputs'][0]['data']['concepts'][0]['id'] == u'second degree burn' and (imageResponse['outputs'][0]['data']['concepts'][0]['value'] > 0.4)):
-    print("Slather your arm in cream cheese and go see a doctor yo!")       
+    print("Slather your arm in cream cheese and go see a doctor yo!")
